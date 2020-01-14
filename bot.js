08718278cf3a588c80847fd22140a0675cb256d7
daaangt <59860240+daaangt@users.noreply.github.com>
2020-01-14 03:09:32 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.login('NjM0NjAxNzg3Mjk0NDgyNDMy.Xh1XGw.5gOdPCeFf8fb8hxucUkqSswyURs');
+client.login('NjM0NjAxNzg3Mjk0NDgyNDMy.Xh1a7A.hUbToWZa_9bcX9dhm1SKfrwr2W4');
