f98c2b2a2b21e6f3fb3db5b84f6c6676747b2123
daaangt <59860240+daaangt@users.noreply.github.com>
2020-01-14 03:02:08 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,17 +3,6 @@ const client = new Discord.Client();
 const config = require("./config.json");
 const snekfetch = require("snekfetch");
 
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
 let status = ['Recrutando', 'WE DO NOT SOW GREYJOY'];
 
 client.on("ready", () => {
